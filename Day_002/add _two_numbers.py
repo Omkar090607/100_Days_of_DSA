70dc0442da8dabdd48dffe97594eff73d7418b16
Omkar090607 <omkarawaze1915@gmail.com>
2026-09-07 20:36:20 +0530

    Day 002 - add_two_numbers.py

---
+++ Day_002/add _two_numbers.py
@@ -0,0 +1,35 @@
+# LeetCode 2: Add Two Numbers
+# Day 002 - 100 Days of DSA
+# Language: Python 3
+# Time Complexity: O(max(m, n))
+# Space Complexity: O(max(m, n))
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        dummy = ListNode(0)
+        current = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            total = val1 + val2 + carry
+            carry = total // 10
+
+            current.next = ListNode(total % 10)
+            current = current.next
+
+            if l1:
+                l1 = l1.next
+
+            if l2:
+                l2 = l2.next
+
+        return dummy.next
