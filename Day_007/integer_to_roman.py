34a0c1dd6fd2ddab04c2491a0f0b016f9bca1274
Omkar090607 <omkarawaze1915@gmail.com>
2026-09-15 22:14:56 +0530

    Day_007: Integer to Roman

---
+++ Day_007/integer_to_roman.py
@@ -0,0 +1,39 @@
+# LeetCode 12 - Integer to Roman
+#
+# Difficulty: Medium
+#
+# Topic: Hash Table, Math, String, Greedy
+#
+# Runtime: 4ms (Beats 60.07%)
+#
+# Memory: 19.24MB (Beats 65.22%)
+
+from typing import List
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        values = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        ]
+
+        symbols = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        ]
+
+        result = []
+
+        # Process values from largest to smallest
+        for value, symbol in zip(values, symbols):
+
+            # Add the symbol while the value can be used
+            while num >= value:
+                result.append(symbol)
+                num -= value
+
+        # Combine all symbols into the final Roman numeral
+        return "".join(result)
+    
