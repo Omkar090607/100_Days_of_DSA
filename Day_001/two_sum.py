3e3f015752ba3e0be583ef8f739d336bc1dcea2b
Omkar090607 <omkarawaze1915@gmail.com>
2026-09-16 22:31:58 +0530

    Day_001: Two Sum

--- Day_001/two_sum.py
+++ Day_001/two_sum.py
@@ -1,3 +1,13 @@
+# LeetCode 1 - Two Sum
+#
+# Difficulty: Easy
+#
+# Topic: Array, Hash Table
+#
+# Runtime: 0ms (Beats 100.00%)
+#
+# Memory: 12.95MB (Beats 82.85%)
+
 class Solution:
     def twoSum(self, nums, target):
         n = len(nums)
@@ -6,4 +16,3 @@ class Solution:
             for j in range(i + 1, n):
                 if nums[i] + nums[j] == target:
                     return [i, j]
-                
